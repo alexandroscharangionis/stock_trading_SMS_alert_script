abdc6172782ac93bce33e8ee89e0c2fbdbbfc792
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-29 16:56:35 +0200

    API setup for stocks & news sources

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+NEWS_API_KEY = "cd74dba04abd4f098e89d2417ee7c59c"
+STOCK_API_KEY = "AMGOWU3MUEBYFY4S"
