9f7cceb51424789875108c88a9094e1c7d703dac
alexandroscharangionis <alexandroscharangionis@gmail.com>
2023-01-29 17:10:25 +0200

    logic for getting yesterday's specified stock price

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = "cd74dba04abd4f098e89d2417ee7c59c"
 STOCK_API_KEY = "AMGOWU3MUEBYFY4S"
+
+# Parameters for stock endpoint request:
+stock_params = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": STOCK,
+    "apikey": STOCK_API_KEY
+}
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for key, value in data.items()]
+yeserday_data = data_list[0]
+yesterday_closing_price = yeserday_data["4. close"]
+print(yesterday_closing_price)
